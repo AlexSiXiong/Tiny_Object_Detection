3186baced9331e28bba3f3f626034c322bd8c459
AlexSiXiong <2428545784@qq.com>
2021-02-12 16:23:15 +1300

    doing my_coco_data

--- my_coco_data.py
+++ my_coco_data.py
@@ -1,5 +1,20 @@
 import json
-import pandas
+import pandas as pd
 import numpy as np
 import os
+import cv2
+
+# file_name, class, xmin, ymin, xmax, ymax
+
+input = pd.read_csv('./try/test.csv')
+
+for row in input.iterrows():
+    row = list(row)[1]
+
+    file_name = row[0]
+    object_class = row[1]
+    x_min = row[2]
+    y_min = row[3]
+    w = row[4] - row[2]
+    h = row[5] - row[3]
 
