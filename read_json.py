98ae61b79ed13282f14be69cdab30db8b94b1b3c
Alexander <2428545784@qq.com>
2021-02-13 20:25:31 +1300

    an example - how to save string into a json file

--- read_json.py
+++ read_json.py
@@ -3,10 +3,14 @@ import json
 with open('./val2017.json', 'r') as f:
     dataset = json.load(f)
     if isinstance(dataset, dict):
+
         for i in dataset.keys():
+            print('----------')
             print(i)
+
             if len(dataset[i]) < 10:
                 print(dataset[i])
             else:
                 print(dataset[i][0])
+                print(dataset[i][1])
                 continue
