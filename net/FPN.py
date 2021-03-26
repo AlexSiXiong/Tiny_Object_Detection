3eb9564ddba78e4e16eb20832dd10b80f8cdd187
Alexander <2428545784@qq.com>
2021-03-27 10:49:23 +1300

    change format

--- net/FPN.py
+++ net/FPN.py
@@ -1,6 +1,10 @@
 import torch.nn as nn
 from torchsummary import summary
 
+"""
+https://blog.csdn.net/weixin_42632271/article/details/107458445?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522161446966516780255296918%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fall.%2522%257D&request_id=161446966516780255296918&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~first_rank_v2~rank_v29-3-107458445.first_rank_v2_pc_rank_v29&utm_term=torchsummary%E5%A4%9Ainput
+"""
+
 
 class FPN(nn.Module):
     def __init__(self, c3_layer, c4_layer, c5_layer, feature_dim=128):
@@ -55,7 +59,4 @@ if __name__ == '__main__':
     # x.append(torch.rand(1, 2048, 19, 19))
     # c = fpn(x)
 
-
     summary(fpn, [(256, 76, 76), (1024, 38, 38), (2048, 19, 19)])
-
-
